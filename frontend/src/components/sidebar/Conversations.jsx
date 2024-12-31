ca6b70bf9e839a7dc5495e1ca0b33d99a008592e
Mahidi Wijayantha <mahidi@thesanmark.com>
2024-12-31 17:11:06 +0530

    #16: Create Messages Component

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -15,23 +15,3 @@ const Conversations = () => {
 }
 
 export default Conversations;
-
-
-//STARTER CODE SNIPPET
-// import React from 'react'
-// import Conversation from './Conversation'
-
-// const Conversations = () => {
-//   return (
-//     <div className='py-2 flex flex-col overflow-auto'>
-//         <Conversation />
-//         <Conversation />
-//         <Conversation />
-//         <Conversation />
-//         <Conversation />
-//         <Conversation />
-//     </div>
-//   )
-// }
-
-// export default Conversations;
