e028e11920eea9608b026b0760e760d56ebfa0cb
Mahidi Wijayantha <mahidi@thesanmark.com>
2025-02-03 09:21:15 +0530

    #26: Implementing Socket.io-1

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -33,7 +33,14 @@ export const sendMessage = async (req, res) => {
         // await conversation.save();
         // await newMessage.save();
 
+        // this will run in parallel
         await Promise.all([conversation.save(),newMessage.save()]);
+
+        // SOCKET IO FUNCTIONALITY WILL GO HERE
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
 
         res.status(201).json(newMessage);
     } catch (error) {
